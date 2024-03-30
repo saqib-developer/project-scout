fc1665f2adead7be7a507dbba120472dac8f97f4
Muhammad Saqib Nawaz <muhammadsaqib8379@gmail.com>
2024-03-31 01:32:51 +0500

    please work

--- app/page.js
+++ app/page.js
@@ -182,7 +182,7 @@ export default function Home() {
       const getdata = async () => {
         const getresponse = await fetch(`/api/singleUser?id=${userUid}`);
 
-        console.log(getresponse.json());
+        console.log(getresponse)
         if (!getresponse.ok) {
           console.error("Failed to fetch data");
         }
